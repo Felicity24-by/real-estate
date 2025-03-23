f26d5f45718c40d16acca792e8b890fa0e032816
Felicity24-by <felicity.sgproperties@gmail.com>
2025-03-23 09:48:20 +0800

    Redux toolkit / reducer / slicer

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { signInFailure, signInStart, signInSuccess } from '../redux/user/userSlice';
 
 export default function SignIn () {
   const [formData, setFormData] = useState ({})
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector ((state) => state.user);
   const navigate = useNavigate();
   const handleChange = (e)  => {
       setFormData({
@@ -16,6 +17,7 @@ export default function SignIn () {
     e.preventDefault();
     setLoading(true);
     try {
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin',
         {
           method:'POST',
@@ -26,16 +28,13 @@ export default function SignIn () {
         });
         const data = await res.json();
         if (data.success === false) {
-          setError(data.message);
-          setLoading(false);
+          dispatch(signInFailure(data.message));
           return;
         }
-        setLoading(false);
-        setError(null);
+        dispatch(signInSuccess(data));
         navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
     return (
